b093d47e67c22153b83ecc7fdcf104bed898e26e
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-12-03 13:10:59 +0100

    add domino deck type and remove set type

---
+++ src/main/kotlin/com/nhcompany/domino/model/DominoDeckType.kt
@@ -0,0 +1,5 @@
+package com.nhcompany.domino.model
+
+enum class DominoDeckType(val number: Int) {
+    DECK_SIX (6)
+}
--- src/main/kotlin/com/nhcompany/domino/model/SetType.kt
+++
@@ -1,5 +0,0 @@
-package com.nhcompany.domino.model
-
-enum class SetType(val number: Int) {
-    SET_SIX (6)
-}
